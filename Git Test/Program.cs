5d02ed2aae501b89bc438fb5036b9a22d3dd84fa
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-01-14 15:41:13 -0600

    Changing "Hello World!" to Guessing Game!

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -10,7 +10,31 @@ namespace Git_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool wrong = true;
+            int count = 0;
+            Random random = new Random();
+            int rand = random.Next(0,100);
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            while (wrong)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+                if (guess < rand)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                }
+                if (guess > rand)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                }
+                if (guess == rand)
+                {
+                    wrong = false;
+                    Console.WriteLine("Correct! You guessed it!");
+                }
+                count++;
+            }
+            Console.WriteLine("You made " + count + " guesses total.");
             return;
         }
     }
